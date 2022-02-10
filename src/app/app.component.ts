319ca56de3efe44db51a9627cfd844c1edbc2e14
ahmedsamir9932 <42872778+ahmedsamir9932@users.noreply.github.com>
2022-02-10 19:06:13 +0200

    making tree is dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,188 +1,119 @@
 import { HttpClient } from '@angular/common/http';
-import {Component,OnInit,ViewEncapsulation} from '@angular/core';
-import {TreeNode} from 'primeng/api';
-import {MessageService} from 'primeng/api';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-    providers: [MessageService],
-    styleUrls: ['./app.component.scss']
+  providers: [MessageService],
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { 
-    data1: TreeNode[];
-    selectedNode: TreeNode;
+export class AppComponent {
+  data1: TreeNode[];
+  selectedNode: TreeNode;
 
-    node = [];
+  node = [];
 
-    constructor(private messageService: MessageService, private http: HttpClient) {}
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
 
-    ngOnInit() {
-        this.http.get('../assets/users.json').subscribe((response: any[]) => {
-            
-
-            var data = response,
-            tree = function (data, root) {
-            var t = {};
-            data.forEach(o => {
-            Object.assign(t[o.id] = t[o.id] || {}, o);
-            t[o.parent] = t[o.parent] || {};
-            t[o.parent].children = t[o.parent].children || [];
-            t[o.parent].children.push(t[o.id]);
+  ngOnInit() {
+    this.http
+      .get('../assets/users.json')
+      .pipe(
+        map((res: any[]) => {
+          let updatedArr = res.map((el) => {
+            return {
+              ...el,
+              parent: el.code.split('.').slice(0, -1).join('.'),
+              id: el.code,
+            };
+          });
+          return updatedArr;
+        })
+      )
+      .subscribe((response) => {
+        response.forEach((el) => {
+          if (el.code.split('.').length <= 1) {
+            delete el.parent;
+          }
         });
-        return t[root].children;
-    }(data, undefined);
 
-    console.log(tree);
-            
-            
-    
-            let mTree = tree.map(el => {
-                return {
-                    label: el.code,
+        var data = response,
+          tree = (function (data, root) {
+            var t = {};
+            data.forEach((o) => {
+              Object.assign((t[o.id] = t[o.id] || {}), o);
+              t[o.parent] = t[o.parent] || {};
+              t[o.parent].children = t[o.parent].children || [];
+              t[o.parent].children.push(t[o.id]);
+            });
+            return t[root].children;
+          })(data, undefined);
+
+        let mTree = tree.map((el) => {
+          return {
+            label: el.code,
+            type: 'person',
+            styleClass: 'p-person',
+            expanded: true,
+            data: {
+              name: el.name,
+              avatar: el.imagePath,
+            },
+            children: el.children?.map((elChild) => {
+              return {
+                label: elChild.code.split('.').slice(-1)[0],
+                type: 'person',
+                styleClass: 'p-person',
+                expanded: true,
+                data: {
+                  name: elChild.name,
+                  avatar: elChild.imagePath,
+                },
+                children: elChild?.children?.map((elChildChild) => {
+                  return {
+                    label: elChildChild.code.split('.').slice(-1)[0],
                     type: 'person',
                     styleClass: 'p-person',
                     expanded: true,
-                    data: 
-                    {
-                    name:el.name, 'avatar': el.imagePath
+                    data: {
+                      name: elChildChild.name,
+                      avatar: elChildChild.imagePath,
                     },
-                    children: el.children?.map(elChild => {
+                    children: elChildChild?.children?.map(
+                      (elChildChildChild) => {
                         return {
-                            label: elChild.code.split('.').slice(-1)[0],
-                            type: 'person',
-                            styleClass: 'p-person',
-                            expanded: true,
-                            data: 
-                            {
-                            name:elChild.name, 'avatar': elChild.imagePath
-                            },  
-                            children: elChild?.children?.map(elChildChild => {
-                                return {
-                                    label: elChildChild.code.split('.').slice(-1)[0],
-                                    type: 'person',
-                                    styleClass: 'p-person',
-                                    expanded: true,
-                                    data: 
-                                    {
-                                    name:elChildChild.name, 'avatar': elChildChild.imagePath
-                                    },
-                                    children: elChildChild?.children?.map(elChildChildChild => {
-                                        return {
-                                            label: elChildChildChild.code.split('.').slice(-1)[0],
-                                            type: 'person',
-                                            styleClass: 'p-person',
-                                            expanded: true,
-                                            data: 
-                                            {
-                                            name:elChildChildChild.name, 'avatar': elChildChildChild.imagePath
-                                            },
-                                        }
-                                    })
-                                }
-                            })
-                        }
-                    })    
-                }
-            })
-            this.data1 = mTree;    
-            
-        })
-        
-        // this.data1 = [{
-        //     label: '0001',
-        //     type: 'person',
-        //     styleClass: 'p-person',
-        //     expanded: true,
-        //     data: 
-        //     {
-        //     name:'ahmed', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'
-        //     },
-        //     children: [
-        //         {
-        //             label: '0003',
-        //             type: 'person',
-        //             styleClass: 'p-person',
-        //             expanded: true,
-        //             data: {name:'ali', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //             children:[
-        //                 {
-        //                     label: '0004',
-        //                     type: 'person',
-        //                     styleClass: 'p-person',
-        //                     expanded: true,
-        //                     data: {name:'ibrahim', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //                     children:[
-        //                         {
-        //                             label: '0002',
-        //                             type: 'person',
-        //                             styleClass: 'p-person',
-        //                             expanded: true,
-        //                             data: {name:'mohamed', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //                         },
-        //                         {
-        //                             label: '0007',
-        //                             type: 'person',
-        //                             styleClass: 'p-person',
-        //                             expanded: true,
-        //                             data: {name:'hany', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //                         },
-        //                     ],
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             label: '0010',
-        //             type: 'person',
-        //             styleClass: 'p-person',
-        //             expanded: true,
-        //             data: {name:'abdullah', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //             children:[
-        //                 {
-        //                     label: '0005',
-        //                     type: 'person',
-        //                     styleClass: 'p-person',
-        //                     expanded: true,
-        //                     data: {name:'mahmoud', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //                     children:[
-        //                         {   // here 
-        //                             label: '0008',
-        //                             type: 'person',
-        //                             styleClass: 'p-person',
-        //                             expanded: true,
-        //                             data: {name:'samir', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //                             children:[
-                                        
-        //                             ]
-        //                         }
-        //                     ]
-        //                 },
-        //                 {
-        //                     label: '0006',
-        //                     type: 'person',
-        //                     styleClass: 'p-person',
-        //                     expanded: true,
-        //                     data: {name:'samy', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //                     children:[
-        //                         {   // here 
-        //                             label: '0009',
-        //                             type: 'person',
-        //                             styleClass: 'p-person',
-        //                             expanded: true,
-        //                             data: {name:'khalid', 'avatar': 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg'},
-        //                             children:[
-                                        
-        //                             ]
-        //                         }
-        //                     ]
-        //                 }
-        //             ]
-        //         }
-        //     ]
-        // }];
-    }
+                          label: elChildChildChild.code.split('.').slice(-1)[0],
+                          type: 'person',
+                          styleClass: 'p-person',
+                          expanded: true,
+                          data: {
+                            name: elChildChildChild.name,
+                            avatar: elChildChildChild.imagePath,
+                          },
+                        };
+                      }
+                    ),
+                  };
+                }),
+              };
+            }),
+          };
+        });
+        this.data1 = mTree;
+      });
+  }
 
-    onNodeSelect(event) {
-        this.messageService.add({severity: 'success', summary: 'Node Selected', detail: event.node.label});
-    }
+  onNodeSelect(event) {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Node Selected',
+      detail: event.node.label,
+    });
+  }
 }
